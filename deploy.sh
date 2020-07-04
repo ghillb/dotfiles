16b0445f3bff700ed5e8b430cdc30c4cabf79b13
GH <ghillebrandt@gmail.com>
2020-07-04 15:47:59 +0200

    adds git branch info to prompt

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ deploy_df()
     cd
     echo ". '~/dotfiles/assets/startup.sh'" >> ~/.profile
     echo -e "# my dotfile additions\n. '~/dotfiles/bashrc'" >> ~/.bashrc
-    sed -i 's/#force_color/force_color/g' ~/.bashrc
+#    sed -i 's/#force_color/force_color/g' ~/.bashrc
     ln -s ~/dotfiles/spectrwm.conf .spectrwm.conf
     ln -s ~/dotfiles/tmux.conf .tmux.conf
     mkdir -p ~/.config/nvim ~/.vim/undodir ; ln -s ~/.vimrc ~/.config/nvim/init.vim; ln -s ~/dotfiles/vimrc .vimrc
