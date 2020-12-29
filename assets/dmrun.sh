61f8fd0727e87e316164e5ec6eab7e01fae29c3b
Gero Hillebrandt <ghillebrandt@gmail.com>
2020-12-29 18:00:09 +0100

    fixes uniconifying

--- assets/dmrun.sh
+++ assets/dmrun.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-dmenu_run -fn 'Fira CodRetinaht-12' -nf '#202020' -nb '#ebdbb2' -sf '#ebdbb2' -sb '#202020'
+dmenu_run -fn 'Fira Code Retina-12' -nf '#202020' -nb '#ebdbb2' -sf '#ebdbb2' -sb '#202020'
+
