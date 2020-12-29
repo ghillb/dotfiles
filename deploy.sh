1975733845cecab8af1163d6d3de70f62f479921
Gero Hillebrandt <ghillebrandt@gmail.com>
2020-12-29 10:42:57 +0100

    adds picom & feh

--- deploy.sh
+++ deploy.sh
@@ -49,7 +49,7 @@ fzf() {
 }
 
 desktop() {
-  sudo apt install -y spectrwm fonts-firacode ttf-ancient-fonts
+  sudo apt install -y spectrwm fonts-firacode ttf-ancient-fonts picom feh
   ln -s $dfdir/spectrwm.conf ~/.spectrwm.conf
   ln -s $dfdir/assets/us_keys_caps_mod /usr/share/X11/xkb/symbols/us_keys_caps_mod
   echo ". '$dfdir/assets/startup.sh'" >> ~/.profile
