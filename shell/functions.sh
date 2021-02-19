2dfa9fd9d62fa70f34d45a1ff19849765cf499d8
Gero Hillebrandt <ghillebrandt@gmail.com>
2021-02-19 21:57:38 +0100

    add docker image hash function

--- shell/functions.sh
+++ shell/functions.sh
@@ -93,3 +93,11 @@ epoch2utc () {
   date -d @"${1}"
 }
 
+hash_docker_image() {
+  docker create $1 | {
+    read cid
+    docker export $cid | tar Oxv 2>&1 | shasum -a 256
+    docker rm $cid > /dev/null
+  }
+}
+
