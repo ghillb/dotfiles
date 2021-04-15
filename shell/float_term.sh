ae5220f0d946854fa6ebb88b2d7d31ab9b6ac5fe
Gero Hillebrandt <ghillebrandt@gmail.com>
2021-04-15 20:29:56 +0200

    launch nvim in floating window

--- shell/float_term.sh
+++ shell/float_term.sh
@@ -3,6 +3,7 @@
 identifier=kitty-float
 active=$(xdotool getactivewindow)
 kitty_float=$(xdotool search --class $identifier)
+if [ $# -gt 0 ]; then kitty_args="bash -ic $@"; else "bash"; fi
 
 if [ -z $kitty_float ]; then
   kitty --class $identifier \
@@ -10,7 +11,7 @@ if [ -z $kitty_float ]; then
     -o initial_window_width=1500 \
     -o initial_window_height=800 \
     -o font_size=14.0 \
-    -e bash
+    -e $kitty_args
 elif [ $active == $kitty_float ]; then
   wmctrl -ir $active -b toggle,hidden
 else
