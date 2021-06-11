c74ffa4fc42e26e2a13c13ac5a35bd32601fd6b7
Gero Hillebrandt <ghillebrandt@gmail.com>
2021-06-11 12:34:33 +0200

    improve autotype

--- shell/autotype.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-xsel -o | tr \\n \\r | xdotool type --clearmodifiers --delay 25 --file -
-# xsel -o | tr \\n \\r | xdotool selectwindow windowfocus type --clearmodifiers --delay 25 --window %@ --file -
-
--- shell/send_cmd.sh
+++ shell/send_cmd.sh
@@ -8,6 +8,12 @@ send_cmd() {
 	tab_del)
     xdotool windowactivate --sync $(xdotool getactivewindow) key Ctrl+F4
 		;;
+	autotype_focus)
+    xsel -o | tr \\n \\r | xdotool type --clearmodifiers --delay 25 --file -
+		;;
+	autotype_target)
+    xsel -o | tr \\n \\r | xdotool selectwindow windowfocus type --clearmodifiers --delay 25 --window %@ --file -
+		;;
 	*)
 		;;
 	esac;
