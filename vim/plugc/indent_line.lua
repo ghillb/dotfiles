e0f2d6dc1e5171800c9122fc0ee1e8bdb81a975f
Gero Hillebrandt <ghillebrandt@gmail.com>
2021-08-03 11:04:42 +0200

    add neoterm to excluded fts

--- vim/plugc/indent_line.lua
+++ vim/plugc/indent_line.lua
@@ -2,5 +2,5 @@ vim.g.indent_blankline_use_treesitter = true
 vim.g.indent_blankline_show_first_indent_level = false
 vim.g.indent_blankline_enabled = true
 vim.g.indent_blankline_show_current_context = true
-vim.g.indent_blankline_filetype_exclude = {'startify', 'vimwiki', 'packer', 'copy-mode', 'help', ''}
+vim.g.indent_blankline_filetype_exclude = {'startify', 'vimwiki', 'packer', 'copy-mode', 'help', 'neoterm', ''}
 
