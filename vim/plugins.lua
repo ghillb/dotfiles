adb05be1b54795e91c14bbc67b2e79d7ea754af1
Gero Hillebrandt <ghillebrandt@gmail.com>
2021-09-03 19:22:39 +0200

    update bootstrap

--- vim/plugins.lua
+++ vim/plugins.lua
@@ -3,10 +3,9 @@ local fn = vim.fn
 
 -- bootstrap packer
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-
-if fn.empty(fn.glob(install_path)) then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd 'packadd packer.nvim'
 end
 
 -- auto compile
