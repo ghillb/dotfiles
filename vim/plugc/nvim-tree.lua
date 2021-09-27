ca0b67c1568775d30b166ffcfc6bcd3ca3ce383e
Gero Hillebrandt <ghillebrandt@gmail.com>
2021-09-27 17:18:49 +0200

    update nvim-tree settings

--- vim/plugc/nvim-tree.lua
+++ vim/plugc/nvim-tree.lua
@@ -3,32 +3,19 @@ if not ok then
   return
 end
 
-vim.g.nvim_tree_side = 'left'
-vim.g.nvim_tree_width = 30
 vim.g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
 vim.g.nvim_tree_gitignore = 1
-vim.g.nvim_tree_auto_open = 0
-vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_auto_ignore_ft = { 'startify', 'dashboard', 'copy-mode'}
 vim.g.nvim_tree_quit_on_open = 0
-vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_hide_dotfiles = 1
 vim.g.nvim_tree_git_hl = 0
 vim.g.nvim_tree_highlight_opened_files = 1
 vim.g.nvim_tree_root_folder_modifier = ':~'
-vim.g.nvim_tree_tab_open = 1
-vim.g.nvim_tree_auto_resize = 0
-vim.g.nvim_tree_disable_netrw = 0
-vim.g.nvim_tree_hijack_netrw = 0
 vim.g.nvim_tree_add_trailing = 1
 vim.g.nvim_tree_group_empty = 1
-vim.g.nvim_tree_lsp_diagnostics = 1
 vim.g.nvim_tree_disable_window_picker = 1
-vim.g.nvim_tree_disable_default_keybindings = 1
-vim.g.nvim_tree_hijack_cursor = 1
 vim.g.nvim_tree_icon_padding = ' '
-vim.g.nvim_tree_update_cwd = 1
 vim.g.nvim_tree_show_icons = { git = 1, folders = 0, files = 0, folder_arrows = 1 }
 vim.g.nvim_tree_window_picker_exclude = { filetype = { packer, qf }, buftype = { terminal } }
 vim.g.nvim_tree_icons = {
@@ -63,7 +50,7 @@ vim.g.nvim_tree_icons = {
 
 local tree_cb = nvimtree_config.nvim_tree_callback
 
-vim.g.nvim_tree_bindings = {
+local list = {
   { key = {"o", "l"},                     cb = tree_cb("edit") },
   { key = "<CR>",                         cb = tree_cb("cd") },
   { key = "<C-v>",                        cb = tree_cb("vsplit") },
@@ -94,5 +81,36 @@ vim.g.nvim_tree_bindings = {
   { key = "]c",                           cb = tree_cb("next_git_item") },
   { key = "q",                            cb = tree_cb("close") },
   { key = "g?",                           cb = tree_cb("toggle_help") },
+}
+
+require'nvim-tree'.setup {
+  disable_netrw       = false,
+  hijack_netrw        = false,
+  open_on_setup       = false,
+  ignore_ft_on_setup  = {},
+  auto_close          = true,
+  open_on_tab         = true,
+  hijack_cursor       = true,
+  update_cwd          = true,
+  lsp_diagnostics     = true,
+  update_focused_file = {
+    enable      = false,
+    update_cwd  = false,
+    ignore_list = {}
+  },
+  system_open = {
+    cmd  = nil,
+    args = {}
+  },
+
+  view = {
+    width = 30,
+    side = 'left',
+    auto_resize = false,
+    mappings = {
+      custom_only = false,
+      list = {}
+    }
+  }
 }
 
