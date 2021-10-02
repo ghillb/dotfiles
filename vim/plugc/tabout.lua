43176c5a019a7bc139e3abab9e1046beb9c36f01
Gero Hillebrandt <ghillebrandt@gmail.com>
2021-10-02 15:45:05 +0200

    make tabout nvim-cmp compatible

--- vim/plugc/tabout.lua
+++ vim/plugc/tabout.lua
@@ -3,8 +3,8 @@ if not ok then
   return
 end
 tabout.setup {
-  tabkey = '<Tab>',
-  backwards_tabkey = '<S-Tab>',
+  tabkey = '',
+  backwards_tabkey = '',
   act_as_tab = true,
   act_as_shift_tab = true,
   enable_backwards = true,
@@ -20,3 +20,32 @@ tabout.setup {
   ignore_beginning = true,
   exclude = {}
 }
+
+-- nvim-cmp compatible bindings
+local function replace_keycodes(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+function _G.tab_binding()
+  if vim.fn.pumvisible() ~= 0 then
+    return replace_keycodes("<C-n>")
+  elseif vim.fn["vsnip#available"](1) ~= 0 then
+    return replace_keycodes("<Plug>(vsnip-expand-or-jump)")
+  else
+    return replace_keycodes("<Plug>(Tabout)")
+  end
+end
+
+function _G.s_tab_binding()
+  if vim.fn.pumvisible() ~= 0 then
+    return replace_keycodes("<C-p>")
+  elseif vim.fn["vsnip#jumpable"](-1) ~= 0 then
+    return replace_keycodes("<Plug>(vsnip-jump-prev)")
+  else
+    return replace_keycodes("<Plug>(TaboutBack)")
+  end
+end
+
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_binding()", {expr = true})
+vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_binding()", {expr = true})
+
