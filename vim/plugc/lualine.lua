961a9934ab5c06eae1d0177fda8e8de4be7ed8bd
Gero Hillebrandt <ghillebrandt@gmail.com>
2021-12-11 13:25:11 +0100

    harmonize diagnostic signs

--- vim/plugc/lualine.lua
+++ vim/plugc/lualine.lua
@@ -1,85 +1,97 @@
-local ok, lualine = pcall(require, 'lualine')
+local ok, lualine = pcall(require, "lualine")
 if not ok then
-  return
+	return
 end
 
-local gruvbox_custom = require'lualine.themes.gruvbox'
+local gruvbox_custom = require("lualine.themes.gruvbox")
 local config = {
-  options = {
-    icons_enabled = true,
-    theme = gruvbox_custom,
-    component_separators = {left = '', right = ''},
-    section_separators = {left = '', right = ''},
-    disabled_filetypes = {'NvimTree'}
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch','g:git_modified_count' ,'diff'},
-    lualine_c = {{'diagnostics', sources = {'nvim_lsp'}}, 'g:breadcrumbs'},
-    lualine_x = {'encoding', 'filetype'},
-    lualine_y = {GetLinePercent},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  extensions = {}
+	options = {
+		icons_enabled = true,
+		theme = gruvbox_custom,
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = { "NvimTree" },
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "g:git_modified_count", "diff" },
+		lualine_c = {
+			{
+				"diagnostics",
+				sources = { "nvim_diagnostic" },
+				symbols = {
+					error = DiagnosticSigns.Error,
+					warn = DiagnosticSigns.Warn,
+					hint = DiagnosticSigns.Hint,
+					info = DiagnosticSigns.Info,
+				},
+			},
+			"g:breadcrumbs",
+		},
+		lualine_x = { "encoding", "filetype" },
+		lualine_y = { GetLinePercent },
+		lualine_z = { "location" },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	extensions = {},
 }
 
 local colors = {
-  black        = '#282828',
-  white        = '#ebdbb2',
-  red          = '#fb4934',
-  green        = '#b8bb26',
-  blue         = '#83a598',
-  yellow       = '#fe8019',
-  pink         = '#b16286',
-  gray         = '#a89984',
-  darkgray     = '#3c3836',
-  lightgray    = '#504945',
-  inactivegray = '#7c6f64',
+	black = "#282828",
+	white = "#ebdbb2",
+	red = "#fb4934",
+	green = "#b8bb26",
+	blue = "#83a598",
+	yellow = "#fe8019",
+	pink = "#b16286",
+	gray = "#a89984",
+	darkgray = "#3c3836",
+	lightgray = "#504945",
+	inactivegray = "#7c6f64",
 }
 
 gruvbox_custom.normal = {
-  a = {bg = colors.gray, fg = colors.black, gui = 'bold'},
-  b = {bg = colors.lightgray, fg = colors.white},
-  c = {bg = colors.darkgray, fg = colors.gray}
+	a = { bg = colors.gray, fg = colors.black, gui = "bold" },
+	b = { bg = colors.lightgray, fg = colors.white },
+	c = { bg = colors.darkgray, fg = colors.gray },
 }
 gruvbox_custom.insert = {
-  a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
-  b = {bg = colors.lightgray, fg = colors.white},
-  c = {bg = colors.darkgray, fg = colors.white}
+	a = { bg = colors.blue, fg = colors.black, gui = "bold" },
+	b = { bg = colors.lightgray, fg = colors.white },
+	c = { bg = colors.darkgray, fg = colors.white },
 }
 gruvbox_custom.visual = {
-  a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
-  b = {bg = colors.lightgray, fg = colors.white},
-  c = {bg = colors.inactivegray, fg = colors.black}
+	a = { bg = colors.yellow, fg = colors.black, gui = "bold" },
+	b = { bg = colors.lightgray, fg = colors.white },
+	c = { bg = colors.inactivegray, fg = colors.black },
 }
 gruvbox_custom.terminal = {
-  a = {bg = colors.pink, fg = colors.black, gui = 'bold'},
-  b = {bg = colors.lightgray, fg = colors.white},
-  c = {bg = colors.darkgray, fg = colors.gray}
+	a = { bg = colors.pink, fg = colors.black, gui = "bold" },
+	b = { bg = colors.lightgray, fg = colors.white },
+	c = { bg = colors.darkgray, fg = colors.gray },
 }
 gruvbox_custom.replace = {
-  a = {bg = colors.red, fg = colors.black, gui = 'bold'},
-  b = {bg = colors.lightgray, fg = colors.white},
-  c = {bg = colors.black, fg = colors.white}
+	a = { bg = colors.red, fg = colors.black, gui = "bold" },
+	b = { bg = colors.lightgray, fg = colors.white },
+	c = { bg = colors.black, fg = colors.white },
 }
 gruvbox_custom.command = {
-  a = {bg = colors.green, fg = colors.black, gui = 'bold'},
-  b = {bg = colors.lightgray, fg = colors.white},
-  c = {bg = colors.inactivegray, fg = colors.black}
+	a = { bg = colors.green, fg = colors.black, gui = "bold" },
+	b = { bg = colors.lightgray, fg = colors.white },
+	c = { bg = colors.inactivegray, fg = colors.black },
 }
 gruvbox_custom.inactive = {
-  a = {bg = colors.darkgray, fg = colors.gray, gui = 'bold'},
-  b = {bg = colors.darkgray, fg = colors.gray},
-  c = {bg = colors.darkgray, fg = colors.gray}
+	a = { bg = colors.darkgray, fg = colors.gray, gui = "bold" },
+	b = { bg = colors.darkgray, fg = colors.gray },
+	c = { bg = colors.darkgray, fg = colors.gray },
 }
 
 lualine.setup(config)
--- vim/plugc/nvim-tree.lua
+++ vim/plugc/nvim-tree.lua
@@ -1,123 +1,128 @@
-local ok, nvimtree_config = pcall(require, 'nvim-tree.config')
+local ok, nvimtree_config = pcall(require, "nvim-tree.config")
 if not ok then
-  return
+	return
 end
 
-vim.g.nvim_tree_auto_ignore_ft = { 'alpha', 'dashboard', 'copy-mode'}
+vim.g.nvim_tree_auto_ignore_ft = { "alpha", "dashboard", "copy-mode" }
 vim.g.nvim_tree_quit_on_open = 0
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_git_hl = 0
 vim.g.nvim_tree_highlight_opened_files = 1
-vim.g.nvim_tree_root_folder_modifier = ':~'
+vim.g.nvim_tree_root_folder_modifier = ":~"
 vim.g.nvim_tree_add_trailing = 1
 vim.g.nvim_tree_group_empty = 1
-vim.g.nvim_tree_icon_padding = ' '
+vim.g.nvim_tree_icon_padding = " "
 vim.g.nvim_tree_show_icons = { git = 1, folders = 0, files = 0, folder_arrows = 1 }
-vim.g.nvim_tree_window_picker_exclude = { filetype = { 'packer', 'qf' }, buftype = { 'terminal' } }
+vim.g.nvim_tree_window_picker_exclude = { filetype = { "packer", "qf" }, buftype = { "terminal" } }
 vim.g.nvim_tree_icons = {
-  default = '', -- ''
-  symlink = '',
-  git = {
-    unstaged = "!",
-    staged = "✓",
-    unmerged = "",
-    renamed = "➜",
-    untracked = "?",
-    deleted = "◯",
-    ignored = "∅"
-  },
-  folder = {
-    arrow_open = "",
-    arrow_closed = "",
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-    symlink_open = "",
-  },
-  lsp = {
-    hint = "",
-    info = "",
-    warning = "",
-    error = "",
-  }
+	default = "", -- ''
+	symlink = "",
+	git = {
+		unstaged = "!",
+		staged = "✓",
+		unmerged = "",
+		renamed = "➜",
+		untracked = "?",
+		deleted = "◯",
+		ignored = "∅",
+	},
+	folder = {
+		arrow_open = "",
+		arrow_closed = "",
+		default = "",
+		open = "",
+		empty = "",
+		empty_open = "",
+		symlink = "",
+		symlink_open = "",
+	},
+	lsp = {
+		hint = "",
+		info = "",
+		warning = "",
+		error = "",
+	},
 }
 
 local tree_cb = nvimtree_config.nvim_tree_callback
 
 local mappings_table = {
-  { key = {"o", "l"},                     cb = tree_cb("edit") },
-  { key = "<CR>",                         cb = tree_cb("cd") },
-  { key = "<C-v>",                        cb = tree_cb("vsplit") },
-  { key = "<C-s>",                        cb = tree_cb("split") },
-  { key = "<C-t>",                        cb = tree_cb("tabnew") },
-  { key = "<C-l>",                        cb = tree_cb("refresh") },
-  { key = "K",                            cb = tree_cb("prev_sibling") },
-  { key = "J",                            cb = tree_cb("next_sibling") },
-  { key = "<",                            cb = tree_cb("first_sibling") },
-  { key = ">",                            cb = tree_cb("last_sibling") },
-  { key = "P",                            cb = tree_cb("parent_node") },
-  { key = "h",                            cb = tree_cb("close_node") },
-  { key = "<BS>",                         cb = tree_cb("dir_up") },
-  { key = "<Tab>",                        cb = tree_cb("preview") },
-  { key = "i",                            cb = tree_cb("toggle_ignored") },
-  { key = ".",                            cb = tree_cb("toggle_dotfiles") },
-  { key = "f",                            cb = tree_cb("create") },
-  { key = "x",                            cb = tree_cb("remove") },
-  { key = "r",                            cb = tree_cb("rename") },
-  { key = "R",                            cb = tree_cb("full_rename") },
-  { key = "m",                            cb = tree_cb("cut") },
-  { key = "c",                            cb = tree_cb("copy") },
-  { key = "p",                            cb = tree_cb("paste") },
-  { key = "y",                            cb = tree_cb("copy_name") },
-  { key = "Y",                            cb = tree_cb("copy_path") },
-  { key = "gy",                           cb = tree_cb("copy_absolute_path") },
-  { key = "[c",                           cb = tree_cb("prev_git_item") },
-  { key = "]c",                           cb = tree_cb("next_git_item") },
-  { key = "q",                            cb = tree_cb("close") },
-  { key = "g?",                           cb = tree_cb("toggle_help") },
+	{ key = { "o", "l" }, cb = tree_cb("edit") },
+	{ key = "<CR>", cb = tree_cb("cd") },
+	{ key = "<C-v>", cb = tree_cb("vsplit") },
+	{ key = "<C-s>", cb = tree_cb("split") },
+	{ key = "<C-t>", cb = tree_cb("tabnew") },
+	{ key = "<C-l>", cb = tree_cb("refresh") },
+	{ key = "K", cb = tree_cb("prev_sibling") },
+	{ key = "J", cb = tree_cb("next_sibling") },
+	{ key = "<", cb = tree_cb("first_sibling") },
+	{ key = ">", cb = tree_cb("last_sibling") },
+	{ key = "P", cb = tree_cb("parent_node") },
+	{ key = "h", cb = tree_cb("close_node") },
+	{ key = "<BS>", cb = tree_cb("dir_up") },
+	{ key = "<Tab>", cb = tree_cb("preview") },
+	{ key = "i", cb = tree_cb("toggle_ignored") },
+	{ key = ".", cb = tree_cb("toggle_dotfiles") },
+	{ key = "f", cb = tree_cb("create") },
+	{ key = "x", cb = tree_cb("remove") },
+	{ key = "r", cb = tree_cb("rename") },
+	{ key = "R", cb = tree_cb("full_rename") },
+	{ key = "m", cb = tree_cb("cut") },
+	{ key = "c", cb = tree_cb("copy") },
+	{ key = "p", cb = tree_cb("paste") },
+	{ key = "y", cb = tree_cb("copy_name") },
+	{ key = "Y", cb = tree_cb("copy_path") },
+	{ key = "gy", cb = tree_cb("copy_absolute_path") },
+	{ key = "[c", cb = tree_cb("prev_git_item") },
+	{ key = "]c", cb = tree_cb("next_git_item") },
+	{ key = "q", cb = tree_cb("close") },
+	{ key = "g?", cb = tree_cb("toggle_help") },
 }
 
-require'nvim-tree'.setup {
-  disable_netrw       = false,
-  hijack_netrw        = false,
-  open_on_setup       = false,
-  ignore_ft_on_setup  = {},
-  auto_close          = true,
-  open_on_tab         = true,
-  hijack_cursor       = true,
-  update_cwd          = true,
-  diagnostics         = {
-    enable = true
-  },
-  update_focused_file = {
-    enable      = true,
-    update_cwd  = true,
-    ignore_list = {}
-  },
-  system_open = {
-    cmd  = nil,
-    args = {}
-  },
-  update_to_buf_dir = {
-    enable = false
-  },
-  view = {
-    width = 30,
-    side = 'left',
-    auto_resize = false,
-    mappings = {
-      custom_only = true,
-      list = mappings_table
-    }
-  },
-  filters = {
-    dotfiles = true,
-    custom = {'.git', 'node_modules', '.cache'}
-  },
-  git = {
-    ignore = true
-  }
-}
-
+require("nvim-tree").setup({
+	disable_netrw = false,
+	hijack_netrw = false,
+	open_on_setup = false,
+	ignore_ft_on_setup = {},
+	auto_close = true,
+	open_on_tab = true,
+	hijack_cursor = true,
+	update_cwd = true,
+	diagnostics = {
+		enable = true,
+		icons = {
+			error = DiagnosticSigns.Error,
+			warning = DiagnosticSigns.Warn,
+			hint = DiagnosticSigns.Hint,
+			info = DiagnosticSigns.Info,
+		},
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+		ignore_list = {},
+	},
+	system_open = {
+		cmd = nil,
+		args = {},
+	},
+	update_to_buf_dir = {
+		enable = false,
+	},
+	view = {
+		width = 30,
+		side = "left",
+		auto_resize = false,
+		mappings = {
+			custom_only = true,
+			list = mappings_table,
+		},
+	},
+	filters = {
+		dotfiles = true,
+		custom = { ".git", "node_modules", ".cache" },
+	},
+	git = {
+		ignore = true,
+	},
+})
