9d245050b42ed2eb827fd63fe8565632e9669d39
Gero Hillebrandt <ghillebrandt@gmail.com>
2021-12-15 22:20:32 +0100

    change stylua config & layouts

--- vim/plugc/null_ls.lua
+++ vim/plugc/null_ls.lua
@@ -1,30 +1,32 @@
 local ok, null_ls = pcall(require, "null-ls")
 if not ok then
-	return
+  return
 end
 
 local opts = {
 
-	sources = {
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.terraform_fmt,
-		null_ls.builtins.formatting.black,
-		null_ls.builtins.formatting.rustfmt,
-		null_ls.builtins.formatting.prettier.with({
-			filetypes = table.merge(null_ls.builtins.formatting.prettier.filetypes, { "docker-compose" }),
-		}),
-	},
+  sources = {
+    null_ls.builtins.formatting.stylua.with({
+      extra_args = { "--config-path", vim.fn.expand("~/.files/configs/stylua.toml") },
+    }),
+    null_ls.builtins.formatting.terraform_fmt,
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.rustfmt,
+    null_ls.builtins.formatting.prettier.with({
+      filetypes = table.merge(null_ls.builtins.formatting.prettier.filetypes, { "docker-compose" }),
+    }),
+  },
 
-	diagnostics_format = "#{m}",
-	debounce = 250,
-	default_timeout = 5000,
-	update_on_insert = false,
-	debug = false,
-	log = {
-		enable = true,
-		level = "warn",
-		use_console = "async",
-	},
+  diagnostics_format = "#{m}",
+  debounce = 250,
+  default_timeout = 5000,
+  update_on_insert = false,
+  debug = false,
+  log = {
+    enable = true,
+    level = "warn",
+    use_console = "async",
+  },
 }
 
 null_ls.setup(opts)
