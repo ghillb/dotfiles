313d33a1d0827091d30f9030adba36f55cac4c55
Gero Hillebrand <35503959+ghillb@users.noreply.github.com>
2022-03-12 20:25:12 +0100

    add make

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -2,7 +2,7 @@
 set -eu
 
 dotfiles=$HOME/.files
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y git
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y git make
 git clone https://github.com/ghillb/dotfiles.git $dotfiles
 cd $dotfiles && make
 
