f61885dab9f59c831367564a526f5442b8f2816f
ghillb <ghillebrandt@gmail.com>
2022-07-21 17:14:19 +0200

    enable in embedded mode

--- neovim/.config/nvim/lua/plugins/comment.lua
+++ neovim/.config/nvim/lua/plugins/comment.lua
@@ -1,6 +1,5 @@
 local packer_opts = {
   "numToStr/Comment.nvim",
-  disable = vim.env.NVIM_EMBEDDED == "true",
   config = function()
     local ok, comment = pcall(require, "Comment")
     if not ok then
