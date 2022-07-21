6f4618a3c57808a44593442f64abcf3366dc0071
ghillb <ghillebrandt@gmail.com>
2022-07-21 17:49:12 +0200

    enable in embedded

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,5 @@
 local packer_opts = {
   "nvim-treesitter/nvim-treesitter",
-  disable = vim.env.NVIM_EMBEDDED == "true",
   requires = { "nvim-treesitter/nvim-treesitter-context", "p00f/nvim-ts-rainbow" },
   config = function()
     local ok, nvim_treesitter_configs = pcall(require, "nvim-treesitter.configs")
