1cca899754f68a8a621aa2682a14b521c7f05e3e
ghillb <ghillebrandt@gmail.com>
2022-10-09 12:05:04 +0200

    simplify lualine  layout

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -66,18 +66,9 @@ local packer_opts = {
         local path_separator = package.config:sub(1, 1)
         return str:gsub(path_separator, breadcrump_sep)
       end,
-      color = { fg = nil, bg = nil, gui = nil },
     }
-    local filename_extension_active = table.deep_copy(filename_extension)
-    local filename_extension_inactive = table.deep_copy(filename_extension)
-    filename_extension_active.color = { fg = _G.palette.ivory, bg = nil, gui = nil }
-    filename_extension_inactive.color = { fg = _G.palette.lightgray, bg = nil, gui = nil }
 
     local aerial_extension = { "aerial", sep = breadcrump_sep, color = { fg = nil, bg = nil, gui = nil } }
-    local aerial_extension_active = table.deep_copy(aerial_extension)
-    local aerial_extension_inactive = table.deep_copy(aerial_extension)
-    aerial_extension_active.color = { fg = _G.palette.magenta, bg = nil, gui = nil }
-    aerial_extension_inactive.color = { fg = _G.palette.pink, bg = nil, gui = nil }
 
     local config = {
       options = {
@@ -158,14 +149,14 @@ local packer_opts = {
       tabline = {},
       winbar = {
         lualine_c = {
-          filename_extension_active,
-          aerial_extension_active,
+          filename_extension,
+          aerial_extension,
         },
       },
       inactive_winbar = {
         lualine_c = {
-          filename_extension_inactive,
-          aerial_extension_inactive,
+          filename_extension,
+          aerial_extension,
         },
       },
 
