2c0b66582c2d4d95b5510e51118e58b5d1f6646d
ghillb <ghillebrandt@gmail.com>
2022-11-19 23:30:17 +0100

    remove custom auto run event

--- neovim/.config/nvim/lua/events.lua
+++ neovim/.config/nvim/lua/events.lua
@@ -91,30 +91,3 @@ aucmd("BufWritePost", {
   group = _binary_edit,
 })
 
--- Auto run on save
-local attach_to_buffer = function(output_bufnr, pattern, command)
-  vim.api.nvim_create_autocmd("BufWritePost", {
-    group = vim.api.nvim_create_augroup("autorun", { clear = true }),
-    pattern = pattern,
-    callback = function()
-      local append_data = function(_, data)
-        if data then
-          vim.api.nvim_buf_set_lines(output_bufnr, -1, -1, false, data)
-        end
-      end
-      vim.api.nvim_buf_set_lines(output_bufnr, 0, -1, false, { "AutoRun:" })
-      vim.fn.jobstart(command, {
-        stdout_buffered = true,
-        on_stdout = append_data,
-        on_stderr = append_data,
-      })
-    end,
-  })
-end
-
-vim.api.nvim_create_user_command("AutoRun", function()
-  local bufnr = vim.fn.input("Enter buffer number: ")
-  local pattern = vim.fn.input("Enter pattern: ")
-  local command = vim.split(vim.fn.input("Enter command: "), " ")
-  attach_to_buffer(tonumber(bufnr), pattern, command)
-end, {})
