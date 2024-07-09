0ec72eaad8e009bdec7d14a773aaa622585b2506
ghillb <ghillebrandt@gmail.com>
2024-07-09 15:24:25 +0200

    fix dependencies

--- .scripts/post_install.sh
+++ .scripts/post_install.sh
@@ -1,22 +1,17 @@
 #!/usr/bin/env bash
 set -eu
 
+mkdir -p "$HOME/code"
+
 # integrate fzf & bash config
 ~/.fzf/install --all
 echo -e "source '$DOTFILES/.bash/bashrc'\n" >>$HOME/.bashrc
 
-# install nvim plugins
-mkdir -p "$HOME/code" # otherwise nvim complains
-pip install --user neovim
-$(which nvim) --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-$(which nvim) --headless +"TSInstallSync all" +"q"
-$(which nvim) --headless +"LspInstall pylsp" +"q"
 
 # add completion
 mkdir ~/.bash_completion.d -p
 curl https://raw.githubusercontent.com/cykerway/complete-alias/master/complete_alias \
   >~/.bash_completion.d/complete_alias
-# activate-global-python-argcomplete{,3} --dest ~/.bash_completion.d/
 
 # configure git
 sudo git config --system pull.ff only
