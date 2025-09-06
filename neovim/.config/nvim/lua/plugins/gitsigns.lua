4f93ba83bc68fb05fdb9758e5c02e79ac31eace4
Gero Hillebrandt <ghillebrandt@gmail.com>
2025-09-06 18:54:17 +0200

    refactor(gitsigns): update key bindings for navigation and actions

--- neovim/.config/nvim/lua/plugins/gitsigns.lua
+++ neovim/.config/nvim/lua/plugins/gitsigns.lua
@@ -52,18 +52,8 @@ return function()
         vim.keymap.set(mode, l, r, opts)
       end
 
-      -- Navigation
-      map('n', ']c', function()
-        if vim.wo.diff then return ']c' end
-        vim.schedule(function() gs.next_hunk() end)
-        return '<Ignore>'
-      end, {expr=true, desc = "Next hunk"})
-
-      map('n', '[c', function()
-        if vim.wo.diff then return '[c' end
-        vim.schedule(function() gs.prev_hunk() end)
-        return '<Ignore>'
-      end, {expr=true, desc = "Previous hunk"})
+      map('n', ']h', gs.next_hunk, {desc = "Next hunk"})
+      map('n', '[h', gs.prev_hunk, {desc = "Previous hunk"})
 
       -- Actions
       map('n', '<leader>hs', gs.stage_hunk, { desc = "Stage hunk" })
