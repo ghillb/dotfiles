095ef35fff70ed8547dfa61a33bcc7b11f9d00ce
Gero Hillebrandt <ghillebrandt@gmail.com>
2025-11-09 10:42:49 +0100

    feat(wezterm): add wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,45 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.font = wezterm.font 'Cascadia Code'
+config.font_size = 11.5
+config.color_scheme = "Bamboo"
+config.default_prog = {"powershell.exe", "-NoLogo"}
+config.hide_tab_bar_if_only_one_tab = true
+
+config.keys = {
+  {
+    key = 'Backspace',
+    mods = 'CTRL',
+    action = wezterm.action.SendKey { key = 'w', mods = 'CTRL' },
+  },
+  {
+    key = 'F11',
+    action = wezterm.action.ToggleFullScreen,
+  },
+  {
+    key = 't',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.ShowLauncher,
+  },
+  {
+    key = '\\',
+    mods = 'CTRL|ALT',
+    action = wezterm.action.SendKey { key = '\\', mods = 'CTRL|ALT' },
+  },
+  {
+    key = 'c',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.CopyTo('Clipboard'),
+  },
+}
+
+wezterm.on('format-window-title', function(tab, pane)
+  return 'tty'
+end)
+
+wezterm.on('window-config-reloaded', function(window, pane)
+  window:toast_notification('WezTerm', 'Configuration reloaded!', nil, 4000)
+end)
+
+return config
