6914e79738b6634031215c72bb4d4761f628fc09
Gero Hillebrandt <ghillebrandt@gmail.com>
2025-11-09 11:25:05 +0100

    config(wezterm): increase font size, change color scheme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,8 +2,9 @@ local wezterm = require("wezterm")
 local config = {}
 
 config.font = wezterm.font 'Cascadia Code'
-config.font_size = 11.5
-config.color_scheme = "Bamboo"
+config.font_size = 12
+-- config.color_scheme = "Bamboo"
+config.color_scheme = 'GruvboxDarkHard'
 config.default_prog = {"powershell.exe", "-NoLogo"}
 config.hide_tab_bar_if_only_one_tab = true
 
