ce244080a9c6a0372e9a8e77c787de9a2d15ac8d
Gero Hillebrandt <ghillebrandt@gmail.com>
2025-11-30 11:57:51 +0100

    feat(nvim): add dap plugin with go and python support

---
+++ neovim/.config/nvim/lua/plugins/dap.lua
@@ -0,0 +1,113 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    keys = {
+      { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle breakpoint" },
+      { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+      { "<leader>di", function() require("dap").step_into() end, desc = "Step into" },
+      { "<leader>do", function() require("dap").step_over() end, desc = "Step over" },
+      { "<leader>dO", function() require("dap").step_out() end, desc = "Step out" },
+      { "<leader>dr", function() require("dap").repl.open() end, desc = "Open REPL" },
+      { "<leader>dl", function() require("dap").run_last() end, desc = "Run last" },
+      { "<leader>dq", function() require("dap").terminate() end, desc = "Terminate" },
+    },
+    config = function()
+      local dap = require("dap")
+
+      -- Go adapter (delve)
+      dap.adapters.delve = function(callback, config)
+        if config.mode == "remote" and config.request == "attach" then
+          callback({
+            type = "server",
+            host = config.host or "127.0.0.1",
+            port = config.port or "38697",
+          })
+        else
+          callback({
+            type = "server",
+            port = "${port}",
+            executable = {
+              command = "dlv",
+              args = { "dap", "-l", "127.0.0.1:${port}" },
+              detached = vim.fn.has("win32") == 0,
+            },
+          })
+        end
+      end
+
+      dap.configurations.go = {
+        {
+          type = "delve",
+          name = "Debug",
+          request = "launch",
+          program = "${file}",
+        },
+        {
+          type = "delve",
+          name = "Debug test",
+          request = "launch",
+          mode = "test",
+          program = "${file}",
+        },
+        {
+          type = "delve",
+          name = "Debug test (go.mod)",
+          request = "launch",
+          mode = "test",
+          program = "./${relativeFileDirname}",
+        },
+      }
+
+      -- Python adapter (debugpy)
+      dap.adapters.python = function(cb, config)
+        if config.request == "attach" then
+          cb({
+            type = "server",
+            port = config.port or 5678,
+            host = config.host or "127.0.0.1",
+          })
+        else
+          cb({
+            type = "executable",
+            command = "python",
+            args = { "-m", "debugpy.adapter" },
+          })
+        end
+      end
+
+      dap.configurations.python = {
+        {
+          type = "python",
+          request = "launch",
+          name = "Launch file",
+          program = "${file}",
+          pythonPath = function()
+            local venv = os.getenv("VIRTUAL_ENV")
+            if venv then
+              return venv .. "/bin/python"
+            end
+            return "python"
+          end,
+        },
+      }
+
+      -- Signs
+      vim.fn.sign_define("DapBreakpoint", { text = "B", texthl = "DiagnosticError" })
+      vim.fn.sign_define("DapStopped", { text = ">", texthl = "DiagnosticInfo", linehl = "Visual" })
+    end,
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-treesitter/nvim-treesitter" },
+    opts = {},
+  },
+  {
+    "igorlfs/nvim-dap-view",
+    dependencies = { "mfussenegger/nvim-dap" },
+    keys = {
+      { "<leader>dt", function() require("dap-view").toggle() end, desc = "Toggle DAP view" },
+      { "<leader>dw", function() require("dap-view").add_expr() end, desc = "Watch expression" },
+    },
+    opts = {},
+  },
+}
