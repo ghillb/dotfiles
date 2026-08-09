a53b7ead82266de68aa537cb862a4e9e99de4d51
Gero Hillebrandt <gero.hillebrandt@pm.me>
2026-08-09 14:08:18 +0200

    feat(neovim): improve Diffview navigation

--- neovim/.config/nvim/lua/plugins/diffview.lua
+++ neovim/.config/nvim/lua/plugins/diffview.lua
@@ -2,4 +2,23 @@ return {
   "sindrets/diffview.nvim",
   cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
   dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local actions = require("diffview.actions")
+    local function close_diffview()
+      require("diffview").close()
+    end
+
+    require("diffview").setup({
+      keymaps = {
+        view = {
+          { "n", "q", close_diffview, { desc = "Close Diffview" } },
+        },
+        file_panel = {
+          { "n", "q", close_diffview, { desc = "Close Diffview" } },
+          { "n", "<C-d>", actions.scroll_view(0.5), { desc = "Scroll the diff down half a page" } },
+          { "n", "<C-u>", actions.scroll_view(-0.5), { desc = "Scroll the diff up half a page" } },
+        },
+      },
+    })
+  end,
 }
